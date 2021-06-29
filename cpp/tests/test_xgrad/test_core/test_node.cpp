eadea4afb1111447344e3dcb1ffd10b7e325b4f1
ctgk <r1135nj54w@gmail.com>
2021-06-29 21:17:54 +0900

    ADD: node class

---
+++ cpp/tests/test_xgrad/test_core/test_node.cpp
@@ -0,0 +1,20 @@
+#include <memory>
+
+#include "xgrad/core.hpp"
+
+#include <CppUTest/TestHarness.h>
+
+TEST_GROUP(node){};
+
+TEST(node, parent)
+{
+    {
+        auto a = xgrad::node();
+        CHECK_TRUE(a.parent() == nullptr);
+    }
+    {
+        auto a = std::make_shared<xgrad::node>();
+        auto b = xgrad::node(a);
+        CHECK_TRUE(b.parent() == a);
+    }
+}
